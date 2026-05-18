7c6b0491e0a8c3ca473c7647f15bc7a4fa529c37
msmith752752 <msmith4587@outlook.com>
2026-05-18 09:02:15 -0400

    Dynamic API_BASE for EC2 vs local

--- static/js/api.js
+++ static/js/api.js
@@ -1,4 +1,6 @@
-const API_BASE = "http://127.0.0.1:8000";
+const API_BASE = (window.location.hostname === "127.0.0.1" || window.location.hostname === "localhost")
+    ? "http://127.0.0.1:8000"
+    : "http://" + window.location.hostname + ":8001";
 
 async function loadDashboard() {
     try {
